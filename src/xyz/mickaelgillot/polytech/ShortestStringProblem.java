35d55394558f32722705043f85ee5b322f8500f3
Coukaratcha <mickael.gillot@gmail.com>
2015-04-15 18:13:46 +0200

    Création des fonction pour permuter une String

--- src/xyz/mickaelgillot/polytech/ShortestStringProblem.java
+++ src/xyz/mickaelgillot/polytech/ShortestStringProblem.java
@@ -5,6 +5,33 @@ import java.util.Set;
 
 public class ShortestStringProblem {
 	
+	public static String changeChar(String s, int pos, char c){
+		return s.substring(0, pos)+c+s.substring(pos+1);
+	}
+	
+	public static String swap(String s){
+		int i = (int) Math.floor((Math.random() * s.length()));
+		int j = (int) Math.floor((Math.random() * s.length()));
+				
+		char c = s.charAt(i);
+		
+		s = changeChar(s, i, s.charAt(j));
+		s = changeChar(s, j, c);
+		
+		return s;
+	}
+	
+	public static String permut(String s, int min, int amplitude){
+		String res = s;
+		
+		int nbIterations = (int) (min + (Math.random() * amplitude));
+		
+		for (int i = 0; i < nbIterations; i++){
+			res = swap(res);
+		}
+		return res;
+	}
+	
 	public static Set<CommonString> monome(String s1, String s2){
 		Set<CommonString> res = new HashSet<CommonString>();
 		
@@ -21,13 +48,12 @@ public class ShortestStringProblem {
 
 	public static void main(String[] args) {
 		// TODO Stub de la méthode généré automatiquement
-		String s1 = "ACTGAGT";
-		String s2 = "TATACGG";
 		
-		Set<CommonString> result = monome(s1, s2);
+		//Set<CommonString> result = monome(s1, s2);
+		String s = "ABCDEFGHI";
+		System.out.println(permut(s, 10, 10));
 		
-		System.out.println(result);
-		
+		//System.out.println(result);		
 	}
 
 }
